792c8a57d81ab288c89af560ffb7e68064705c69
Rone <rone_sigismundo@hotmail.com>
2021-01-26 15:13:04 -0300

    Corrigindo Bug da tag og:image

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -2,18 +2,17 @@ import { loadGetInitialProps } from 'next/dist/next-server/lib/utils'
 import Head from 'next/head'
 
 
-function headIndexPage(props) {
+export default function headIndexPage(props) {
     return (
         <div>
             <Head>
                 <title>Quiz World</title>
                 <meta property="og:title" content="Quiz World" key="title" />
-                <meta property="og:description" content="Uma aplicação feita na imersão React da Alura e a cada dia ganha mais forma" key="description" />
+                <meta property="og:description" content="Uma aplicação feita na semana de imersão React da Alura e a cada dia ganha mais forma." key="description" />
                 <meta property="og:image" content={props.image} key="image" />
             </Head>
         </div>
     )
 }
 
-export default headIndexPage
 
